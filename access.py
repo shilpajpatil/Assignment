b88bf8bc828f7b3a03dbfaa456e711d423ee2f6a
shilpajpatil <40339744+shilpajpatil@users.noreply.github.com>
2019-10-24 11:33:00 +0530

    question1
    create a module and import this module into another file.

---
+++ access.py
@@ -0,0 +1,15 @@
+import module
+number1=int(input("enter a number"))
+number2=int(input("enter second numebr"))
+
+result=module.add(number1,number2)
+print("addition is:",result)
+
+result=module.sub(number1,number2)
+print("subtraction is:",result)
+
+result=module.mul(number1,number2)
+print("multiplication is:",result)
+
+result=module.div(number1,number2)
+print("division is:",result)
