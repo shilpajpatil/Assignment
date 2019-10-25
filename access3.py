2ebe168d16d91db92013e82528042b4fabff80d8
shilpajpatil <40339744+shilpajpatil@users.noreply.github.com>
2019-10-25 14:39:15 +0530

    question3
    factorial of number

---
+++ access3.py
@@ -0,0 +1,17 @@
+import module3
+
+num1=int(input("enter a number"))
+factorial=module3.fact(num1)
+print("factorial is:",factorial)
+
+
+
+
+"""import module7
+
+row=int(input("enter number of rows"))
+column=int(input("enter numebr of columns"))
+
+module7.pattern(row,column)
+
+"""
---
+++ module3.py
@@ -0,0 +1,20 @@
+
+def fact(no1):
+    res=1
+    for i in range(1,no1+1):
+        res=res*i
+    return res
+
+
+
+
+"""
+def pattern(irow,icol):
+    for i in range (irow):
+        for j in range (icol):
+            if((irow==icol) or (irow>icol)):
+                print( i ,end=' ')
+            elsif():
+                pass
+        print()
+"""
