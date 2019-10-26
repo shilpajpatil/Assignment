1ba71e57c8e1fd8bf3a60753de4f64be4813a6bc
shilpajpatil <40339744+shilpajpatil@users.noreply.github.com>
2019-10-26 22:22:17 +0530

    question6
    pattern of *

---
+++ assign2.6.py
@@ -0,0 +1,4 @@
+import module26
+
+number=int(input("enter a number"))
+module26.numpattern8(number)
---
+++ module26.py
@@ -0,0 +1,6 @@
+def numpattern8(number):
+    for row in range(1, number):
+        for column in range(1, number):
+            if((row==column) or (row<=column)):
+                print('*', end=' ')
+        print("")
